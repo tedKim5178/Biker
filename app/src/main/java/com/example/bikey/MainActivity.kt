368f0a3789a9e5ae6bbb4e3e88747d85ba875a5f
moonki-kim <moonki.kim@navercorp.com>
2020-04-18 14:53:42 +0900

    코드 정리

--- app/src/main/java/com/example/bikey/MainActivity.kt
+++ app/src/main/java/com/example/bikey/MainActivity.kt
@@ -10,37 +10,31 @@ import android.location.LocationManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity(), LocationListener{
 
-    private lateinit var tv_speed: TextView
+    private lateinit var speedTv: TextView
     private var lastLocation: Location? = null
     private var calculatedSpeed = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_speed = findViewById(R.id.speed)
+        speedTv = findViewById(R.id.speed)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
             && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED) {
-            // TODO :: string array
             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
 
         } else {
-            // TODO :: do stuff
-            // start the program when permission granted
-            doStuff()
+            requestLocationUpdates()
         }
     }
 
     @SuppressLint("MissingPermission")
-    private fun doStuff() {
+    private fun requestLocationUpdates() {
         val locationManager = (getSystemService(Context.LOCATION_SERVICE) as? LocationManager)
-        // 1초에 한번씩 업데이트, 최소 미터 변화는 1미터
         locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1f, this)
         locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1f, this)
     }
@@ -48,13 +42,13 @@ class MainActivity : AppCompatActivity(), LocationListener{
     override fun onLocationChanged(location: Location?) {
         if (location != null && lastLocation != null) {
             val time = (location.time - lastLocation!!.time) / 1_000f
-            // TODO :: check time < 0
+            if (time < 0) return
             // TODO :: 0.1 자리 까지 바꾸기
             calculatedSpeed = (lastLocation!!.distanceTo(location) / time).toInt()
         }
         lastLocation = location
         val speed = if (location?.hasSpeed() == true) location.speed else calculatedSpeed
-        tv_speed.text = speed.toString()
+        speedTv.text = speed.toString()
     }
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
@@ -74,7 +68,7 @@ class MainActivity : AppCompatActivity(), LocationListener{
         if (requestCode == 1000) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // do Stuff
-                doStuff()
+                requestLocationUpdates()
             } else {
                 finish()
             }
