e12a7f86353ecf5ea85baacb3579890dce5e40da
moonki-kim <moonki.kim@navercorp.com>
2020-12-04 22:11:55 +0900

    feature(firebase_auth) : apply firebase auth with sign in anonymously

--- app/src/main/java/com/mk/bikey/MainActivity.kt
+++ app/src/main/java/com/mk/bikey/MainActivity.kt
@@ -6,6 +6,9 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.mk.bikey.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,5 +23,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        Firebase.auth.signInAnonymously()
     }
 }
